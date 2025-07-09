9170b8c0cbf077f19e0ce12590421d23a7d53942
Arthur Graciliano <78110101+Arthurc0mH@users.noreply.github.com>
2025-07-09 09:12:52 -0300

    batalha naval
    se não for eu coloco dps

--- main.c
+++ main.c
@@ -5,12 +5,15 @@ void imprimir(int tabuleiro1[6][6], int *jogador, int tabuleiro2[6][6]);
 void colocar_pecas(int tabuleiro1[6][6], int *jogador, int pecas1[3], int plataforma, int tabuleiro2[6][6], int pecas2[3]);
 void limpar_tela(int plataforma);
 void converter_y(int *y2, char *y);
+void aa(int a[2], int tabuleiro1[6][6], int tabuleiro2[6][6]); //essa é a função pra setar quantas peças cada jogador jogou
+void jogo(int plataforma, int tabuleiro1[6][6], int tabuleiro2[6][6], int *jogador, int a[2], int pontos[2]);
 
 int main(){
     int tabuleiro1[6][6]; int tabuleiro2[6][6];
-    int pecas1[3]; int pecas2[3];
+    int pecas1[3]; int pecas2[3]; int pontos[2]; int a[2]; //a é só pra guardar quantas peças cada jogador jogou
     int fim = 0; int jogador = 0; int plataforma;
     pecas1[0] = 3; pecas1[1] = 2; pecas1[2] = 1; pecas2[0] = 3; pecas2[1] = 2; pecas2[2] = 1;
+    
     for(int i = 0; i < 6; i++){
         if(i < 3){
         }
@@ -23,8 +26,8 @@ int main(){
     scanf("%d", &plataforma);
     if(plataforma == 3){
         printf("modo debug\n");
-        pecas1[0] = 1; pecas1[1] = 1; pecas1[2] = 1; pecas2[0] = 1; pecas2[1] = 1; pecas2[2] = 1;
-        plataforma = 1;
+        pecas1[0] = 1; pecas1[1] = 0; pecas1[2] = 0; pecas2[0] = 1; pecas2[1] = 0; pecas2[2] = 0;
+        plataforma = 2; //1 pra windows, qualquer outra coisa pra linux
     }
     limpar_tela(plataforma);
 
@@ -33,6 +36,10 @@ int main(){
         limpar_tela(plataforma);
         jogador = 1;
         colocar_pecas(tabuleiro1, &jogador, pecas1, plataforma, tabuleiro2, pecas2);
+        jogador = 2;
+        aa(a, tabuleiro1, tabuleiro2);
+        limpar_tela(plataforma);
+        jogo(plataforma, tabuleiro1, tabuleiro2, &jogador, a, pontos);
 
         fim++;
     }
@@ -66,7 +73,20 @@ void imprimir(int tabuleiro1[6][6], int *jogador, int tabuleiro2[6][6]){
                     printf("0  ");
                 }else if(tabuleiro1[i][j] == 1){
                     printf("1  ");
+                }else if(tabuleiro1[i][j] == 2){
+                    printf("1X ");
+                }else if(tabuleiro1[i][j] == 3){
+                    printf("1E ");
+                }else if(tabuleiro1[i][j] == 4){
+                    printf("D  ");
+                }else if(tabuleiro1[i][j] == 5){
+                    printf("DX ");
+                }else if(tabuleiro1[i][j] == 6){
+                    printf("DE ");
+                }else if(tabuleiro1[i][j] == 7){
+                    printf("E  ");
                 }
+                
             }
             printf(" ");
             printf("\n");
@@ -95,7 +115,19 @@ void imprimir(int tabuleiro1[6][6], int *jogador, int tabuleiro2[6][6]){
                 if(tabuleiro2[i][j] == 0){
                     printf("0  ");
                 }else if(tabuleiro2[i][j] == 1){
-                    printf("2  ");
+                    printf("1  ");
+                }else if(tabuleiro2[i][j] == 2){
+                    printf("1X ");
+                }else if(tabuleiro2[i][j] == 3){
+                    printf("1E ");
+                }else if(tabuleiro2[i][j] == 4){
+                    printf("D  ");
+                }else if(tabuleiro2[i][j] == 5){
+                    printf("DX ");
+                }else if(tabuleiro2[i][j] == 6){
+                    printf("DE ");
+                }else if(tabuleiro2[i][j] == 7){
+                    printf("E  ");
                 }
             }
             printf(" ");
@@ -340,4 +372,105 @@ void converter_y(int *y2, char *y){
     }else if(*y == 'f' || *y == 'F'){
         *y2 = 5;
     }   
+}
+
+void aa(int a[2], int tabuleiro1[6][6], int tabuleiro2[6][6]){
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 6; j++){
+            if(tabuleiro1[i][j] == 1){
+                a[0]++;
+            }
+            if(tabuleiro2[i][j] == 1){
+                a[1]++;
+            }
+        }
+    }
+}
+
+void jogo(int plataforma, int tabuleiro1[6][6], int tabuleiro2[6][6], int *jogador, int a[2], int pontos[2]){
+    int temp = 0; int x; int y2;
+    char y;
+    int turno0 = 0;
+
+    while(temp == 0){
+        if(turno0 == 0){
+            if(*jogador == 2){ //jogador1
+                limpar_tela(plataforma);
+                printf("Pontos jogador 1: %d\nPontos jogador 2: %d\n", pontos[0], pontos[1]);
+                imprimir(tabuleiro1, jogador, tabuleiro2);
+                printf("Escolha onde você quer atacar. (linha, coluna. Ex: 1a)\n");
+                scanf("%d %c", &x, &y);
+                converter_y(&y2, &y);
+                if(tabuleiro2[x-1][y2] == 0){ //errou
+                    if(tabuleiro1[x-1][y2] == 0){
+                        tabuleiro1[x-1][y2] = 4; //0 -> E
+                    }else if(tabuleiro1[x-1][y2] == 1){
+                        tabuleiro1[x-1][y2] = 3; //1 -> 1E
+                    }else if(tabuleiro1[x-1][y2] == 4){
+                        tabuleiro1[x-1][y2] = 6; //D -> DE
+                    }
+                    *jogador = 3;
+                }else if(tabuleiro2[x-1][y2] == 1 || tabuleiro2[x-1][y2] == 2 || tabuleiro2[x-1][y2] == 3){ //acertou
+                    if(tabuleiro1[x-1][y2] == 0){
+                        tabuleiro1[x-1][y2] = 2; //0 -> X
+                    }else if(tabuleiro1[x-1][y2] == 1){
+                        tabuleiro1[x-1][y2] = 2; //1 -> 1X
+                    }
+                    if(tabuleiro2[x-1][y2] == 1){
+                        tabuleiro2[x-1][y2] = 4; //1 -> D
+                    }else if(tabuleiro2[x-1][y2] == 2){
+                        tabuleiro2[x-1][y2] = 5; //1X -> DX
+                    }else if(tabuleiro2[x-1][y2] == 3){
+                        tabuleiro2[x-1][y2] = 6; //1E -> DE
+                    }
+                    pontos[0]++;
+                    if(pontos[0] == a[0]){
+                        turno0++; //caso o jogador ganhe antes de poder jogar de novo
+                    }
+                }
+            }else if(*jogador == 3){ //jogador2
+                limpar_tela(plataforma);
+                printf("Pontos jogador 1: %d\nPontos jogador 2: %d\n", pontos[0], pontos[1]);
+                imprimir(tabuleiro1, jogador, tabuleiro2);
+                printf("Escolha onde você quer atacar. (linha, coluna. Ex: 1a)\n");
+                scanf("%d %c", &x, &y);
+                converter_y(&y2, &y);
+                if(tabuleiro1[x-1][y2] == 0){ //errou
+                    if(tabuleiro2[x-1][y2] == 0){
+                        tabuleiro2[x-1][y2] = 4; //0 -> E
+                    }else if(tabuleiro2[x-1][y2] == 1){
+                        tabuleiro2[x-1][y2] = 3; //1 -> 1E
+                    }else if(tabuleiro2[x-1][y2] == 4){
+                        tabuleiro2[x-1][y2] = 6; //D -> DE
+                    }
+                    *jogador = 2;
+                }else if(tabuleiro1[x-1][y2] == 1 || tabuleiro1[x-1][y2] == 2 || tabuleiro1[x-1][y2] == 3){ //acertou
+                    if(tabuleiro2[x-1][y2] == 0){
+                        tabuleiro2[x-1][y2] = 2; //0 -> X
+                    }else if(tabuleiro2[x-1][y2] == 1){
+                        tabuleiro2[x-1][y2] = 2; //1 -> 1X
+                    }
+                    if(tabuleiro1[x-1][y2] == 1){
+                        tabuleiro1[x-1][y2] = 4; //1 -> D
+                    }else if(tabuleiro1[x-1][y2] == 2){
+                        tabuleiro1[x-1][y2] = 5; //1X -> DX
+                    }else if(tabuleiro1[x-1][y2] == 3){
+                        tabuleiro1[x-1][y2] = 6; //1E -> DE
+                    }
+                    pontos[1]++;
+                    if(pontos[1] == a[1]){
+                        turno0++; //caso o jogador ganhe antes de poder jogar de novo
+                    }
+                }
+            }
+        }else{
+            if(pontos[0] == a[0]){
+                printf("Jogador 1 Venceu!\n");
+                temp++;
+            }else if(pontos[1] == a[1]){
+                printf("Jogador 2 Venceu!\n");
+                temp++;
+            }
+        }
+    }
 }
